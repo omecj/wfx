e95b3b73bf00d9cbff8f48520a9d681e261e560a
oumaecj <dingxaingcj@github.com>
2019-07-31 15:56:30 +0800

    version 1.0

--- admin/src/main/java/com/qf/admin/controller/GoodsController.java
+++ admin/src/main/java/com/qf/admin/controller/GoodsController.java
@@ -1,11 +1,15 @@
 package com.qf.admin.controller;
 
+import com.qf.common.http.Result;
 import com.qf.service.impl.GoodsServiceImpl;
 import com.qf.service.impl.GoodsTypeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author-izumi
@@ -25,5 +29,26 @@ public class GoodsController {
         model.addAttribute("goodsTypes", goodsTypeService.list());
         model.addAttribute("goodsList", goodsService.list());
         return "goods/list";
+    }
+
+    @PostMapping("up")
+    @ResponseBody
+    public Result<?> toUp(@RequestParam(value = "id",required = true) Long id){
+
+        return Result.success();
+    }
+
+    @PostMapping("up")
+    @ResponseBody
+    public Result<?> toDown(@RequestParam(value = "id",required = true) Long id){
+
+        return Result.success();
+    }
+
+    @PostMapping("up")
+    @ResponseBody
+    public Result<?> toDel(@RequestParam(value = "id",required = true) Long id){
+
+        return Result.success();
     }
 }
